5d9d4e7edc27d92c8c678dc6ecf77e2e2d6c5f09
Lygon Bowen-West <lygon@yobota.xyz>
2019-06-18 11:02:57 +0100

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,7 +4,7 @@ from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 
-MAX_WAIT = 10
+MAX_WAIT = 5
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -90,7 +90,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Francis visits the homepage. There is no sign of Edit's list
         self.browser.get(self.live_server_url)
-        page_text = self.browser.find_elements_by_tag_name("body").text
+        page_text = self.browser.find_element_by_tag_name("body").text
         self.assertNotIn("Buy peacock feathers", page_text)
         self.assertNotIn("make a fly", page_text)
 
